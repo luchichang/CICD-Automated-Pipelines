b33e6d9cf7f7133252d58643a3476c2d266b758b
Ubuntu <ubuntu@ip-172-31-41-208.ap-south-1.compute.internal>
2024-08-25 15:38:26 +0000

    first Shared Library file

---
+++ viaJenkins/vars/sharedLibInfo.groovy
@@ -0,0 +1,4 @@
+def slInfo() {
+  //executing using the shell script
+  sh' echo  shared Libraries are used in jenkins inorder to use Code reusability, Standardization of Pipeline, one place to fix issues with shared or common code, Code Maintenance. ' 
+}
